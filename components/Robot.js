b6c175edd90e3300e01dd514abbf1dd4ba504bb4
Sandipan03 <sandipan2018@yahoo.com>
2024-08-09 15:09:39 +0530

    commit2

--- components/Robot.js
+++ components/Robot.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Spline from '@splinetool/react-spline';
 function Robot({className}) {
   return (
-    <div className={className}>
+    <div className={className} style={{width:"100vw",height: "100vh"}}>
         <Spline scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode" />
   
 
