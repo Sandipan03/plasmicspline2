f190b17bef3d460ec59feff785deac25cc7b3852
Sandipan03 <sandipan2018@yahoo.com>
2024-08-09 17:12:44 +0530

    commit5

--- components/Robot.js
+++ components/Robot.js
@@ -1,30 +1,46 @@
 /**@format */
-import React from 'react'
+import React, { useState } from 'react';
 import Spline from '@splinetool/react-spline';
 import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-function Robot({className}) {
-    useGSAP(()=>{
-        gsap.to(".loader",{
-            opacity: 0,
-            duration: 1,
-            delay: 1,
-            rotate: 180,
-        })
-        gsap.from(".spline",{
-            opacity: 0,
-            duration: 1,
-            delay: 2,
-        })
-    })
-  return (
-    <div className={className} style={{width:"100vw",height: "100vh"}}>
-        <h1 className="loader" style={{fontSize:"40px",position: "absolute", marginLeft: "45vw", marginTop: "40vh"}}>Loading...</h1>
-        <Spline className="spline" scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode" />
-  
 
+function Robot({ className }) {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+    gsap.to(".loader", {
+      opacity: 0,
+      duration: 1,
+      rotate: 180,
+    });
+    gsap.from(".spline", {
+      opacity: 0,
+      duration: 1,
+    });
+  };
+
+  return (
+    <div className={className} style={{ width: '100vw', height: '100vh' }}>
+      {loading && (
+        <h1
+          className="loader"
+          style={{
+            fontSize: '40px',
+            position: 'absolute',
+            marginLeft: '45vw',
+            marginTop: '40vh',
+          }}
+        >
+          Loading...
+        </h1>
+      )}
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode"
+        onLoad={handleLoad}
+      />
     </div>
-  )
+  );
 }
 
-export default Robot
+export default Robot;
