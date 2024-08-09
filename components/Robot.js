03f7d0e8e59ac67403196bd948f340e692246db2
Sandipan03 <sandipan2018@yahoo.com>
2024-08-09 14:56:42 +0530

    commit1

---
+++ components/Robot.js
@@ -0,0 +1,14 @@
+/**@format */
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+function Robot({className}) {
+  return (
+    <div className={className}>
+        <Spline scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode" />
+  
+
+    </div>
+  )
+}
+
+export default Robot
