d68d6dbd8fda1c34b9b15fa8e7d8b314719c8955
Sandipan03 <sandipan2018@yahoo.com>
2024-08-09 15:26:55 +0530

    commit3

--- components/Robot.js
+++ components/Robot.js
@@ -1,10 +1,26 @@
 /**@format */
 import React from 'react'
 import Spline from '@splinetool/react-spline';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 function Robot({className}) {
+    useGSAP(()=>{
+        gsap.to(".loader",{
+            opacity: 0,
+            duration: 1,
+            delay: 1,
+            rotate: 180,
+        })
+        gsap.from(".spline",{
+            opacity: 0,
+            duration: 1,
+            delay: 2,
+        })
+    })
   return (
     <div className={className} style={{width:"100vw",height: "100vh"}}>
-        <Spline scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode" />
+        <h1 className="loader" style={{position: "absolute", marginLeft: "40vw", marginTop: "40vh"}}>Loading...</h1>
+        <Spline className="spline" scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode" />
   
 
     </div>
