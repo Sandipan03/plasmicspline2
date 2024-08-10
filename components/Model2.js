e970e540275ba5eb8c747421413075cbce7f8a3a
Sandipan03 <sandipan2018@yahoo.com>
2024-08-10 11:53:46 +0530

    commit9

--- components/Model2.js
+++ components/Model2.js
@@ -1,33 +1,46 @@
 /**@format */
-import React, { Suspense } from "react";
-const Spline = React.lazy(() => import("@splinetool/react-spline"));
+import React, { useState } from 'react';
+import Spline from '@splinetool/react-spline';
+import gsap from 'gsap';
 
-function Robot({ className }) {
- 
+function Model2({ className }) {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+    gsap.to(".loader", {
+      opacity: 0,
+      duration: 1,
+      
+    });
+    gsap.from(".spline", {
+      opacity: 0,
+      duration: 1,
+    });
+  };
 
   return (
+    <div className={className} style={{ width: '100vw', height: '100vh' }}>
       
-        
-       <div className={className} style={{ width: "100vw", height: "100vh" }}>
-      <Suspense
-        fallback={
-          <h1
-            className='loader'
-            style={{
-              fontSize: "40px",
-              position: "absolute",
-              marginLeft: "45vw",
-              marginTop: "40vh",
-            }}
-          >
-            Loading....
-          </h1>
-        }
-      >
-        <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' />
-      </Suspense>
+        <h1
+          className="loader"
+          style={{
+            fontSize: '40px',
+            position: 'absolute',
+            marginLeft: '45vw',
+            marginTop: '40vh',
+          }}
+        >
+          Loading....
+        </h1>
+     
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/id5se5REy17fe7RR/scene.splinecode"
+        onLoad={handleLoad}
+      />
     </div>
   );
 }
 
-export default Robot;
+export default Model2;
