b25308c135e5c660b54076b679e03447d6fbc2dd
Sandipan03 <sandipan2018@yahoo.com>
2024-08-12 17:50:31 +0530

    commit10

--- components/Model2.js
+++ components/Model2.js
@@ -36,7 +36,7 @@ function Model2({ className }) {
      
       <Spline
         className="spline"
-        scene="https://prod.spline.design/DoeRyB08RDh7hUMG/scene.splinecode"
+        scene="https://prod.spline.design/iewzFp7qftg-N4ru/scene.splinecode"
         onLoad={handleLoad}
       />
     </div>
